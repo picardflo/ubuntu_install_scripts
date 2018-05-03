89ca1e147a63b727e615683c5e5fa78415bcd68e
picardflo <picard.flo@gmail.com>
2018-05-03 20:53:10 +0200

    add Nextcloud & qownnotes et check version of untity and gnome tweek tools.

--- install.sh
+++ install.sh
@@ -16,92 +16,114 @@ if [ $(id -u) -eq 0 ];
 		sleep 5
 
 		## From official Ubuntu Repositories
-		apt install filezilla -y
-		apt install mc -y
-		apt install vlc -y
-		apt install cifs-utils -y
-		apt install ssh -y
-		apt install conky -y
-		apt install gpodder -y
-		apt install remmina -y
-		apt install remmina-plugin-* -y
-		apt install vim -y
-		apt install git -y
-		apt install tree -y
-		apt install zip -y
-		apt install nmap -y
-		apt install htop -y
-		apt install digikam -y
-		apt install shotwell -y
-		apt install gparted -y
-		apt install make -y
-		apt install gcc -y
-		apt install slurm -y
-		apt install zip -y
-		apt install keepass2 -y
-		apt install bmon -y
-		apt install iftop -y
-		apt install dos2unix -y
-		apt install mysql-client -y
-		apt install packagekit -y
-		apt install apt-transport-https -y
-		apt install hplip -y
-		apt install hardinfo -y
-		apt install redshift-gtk -y
-		apt install easytag -y
-		apt install brasero -y
-		apt install acpi -y
-		apt install xpad -y
-		apt install unity-tweak-tool -y
+		apt install filezilla --yes
+		apt install mc --yes
+		apt install vlc --yes
+		apt install cifs-utils --yes
+		apt install nfs-utils --yes
+		apt install ssh --yes
+		apt install conky --yes
+		apt install gpodder --yes
+		apt install remmina --yes
+		apt install remmina-plugin-* --yes
+		apt install vim --yes
+		apt install git --yes
+		apt install tree --yes
+		apt install zip --yes
+		apt install nmap --yes
+		apt install htop --yes
+		apt install digikam --yes
+		apt install shotwell --yes
+		apt install gparted --yes
+		apt install make --yes
+		apt install gcc --yes
+		apt install slurm --yes
+		apt install zip --yes
+		apt install keepass2 --yes
+		apt install bmon --yes
+		apt install iftop --yes
+		apt install dos2unix --yes
+		apt install mysql-client --yes
+		apt install packagekit --yes
+		apt install apt-transport-https --yes
+		apt install hplip --yes
+		apt install hardinfo --yes
+		apt install redshift-gtk --yes
+		apt install easytag --yes
+		apt install brasero --yes
+		apt install acpi --yes
+		apt install xpad --yes
+		apt install bleachbit --yes
+		apt install screenfetch --yes
 
 
-    ## From ppa or externa Repositories
+		# Check version and install tools by desktop env.
+		if [ $(pgrep -c gnome-panel) -gt 0 ];
+		then
+			apt install unity-tweak-tool --yes	
+		else
+			apt install gnome-tweak-tool --yes
+		
+		fi
+
+
+   		## From ppa or externa Repositories
 
 		# Chrome (stable)
 		wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 		sh -c ‘echo « deb https://dl.google.com/linux/chrome/deb/ stable main » > /etc/apt/sources.list.d/google-chrome.list’
 		apt update
-                apt install google-chrome-stable -y
+                apt install google-chrome-stable --yes
 
 		# Atom
 		add-apt-repository ppa:webupd8team/atom --yes
 		apt update
-		apt install atom -y
+		apt install atom --yes
 
 		# Peek - Screen Recorder (Gif)
 		sudo add-apt-repository ppa:peek-developers/stable --yes
 		sudo apt update
-		sudo apt install peek -y
+		sudo apt install peek --yes
 
 		# grub-customizer
 		add-apt-repository --yes ppa:danielrichter2007/grub-customizer
-		apt update &&  apt install -y grub-customizer
+		apt update &&  apt install --yes grub-customizer
 
 		# install conky Manager
 		add-apt-repository ppa:teejee2008/ppa --yes
 		apt update
-		apt install conky-manager -y
+		apt install conky-manager --yes
 
 
 		# install HandBrake
 		apt-add-repository ppa:stebbins/handbrake-releases --yes
 		apt update
-		apt install handbrake -y
+		apt install handbrake --yes
 		# simple library designed for accessing DVDs like a block device without having to bother about the decryption (DRM bypass)
-		#apt install libdvdcss2 -y
+		#apt install libdvdcss2 --yes
 
 		# virtualbox
 		sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -c -s) contrib" > /etc/apt/sources.list.d/oracle-virtualbox5.list'
 		wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 		wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 		apt update
-		apt install virtualbox-5.1 -y
+		apt install virtualbox-5.1 --yes
 
 
 		#Caffeine
 		add-apt-repository ppa:caffeine-developers/ppa --yes
 		apt-get update
-		apt install caffeine -y
+		apt install caffeine --yes
+
+		#qownnotes
+		add-apt-repository ppa:pbek/qownnotes --yes
+		apt-get update
+		apt-get install qownnotes --yes
+
+		#
+		add-apt-repository ppa:nextcloud-devs/client
+		apt-get update
+		apt-get install nextcloud-client --yes
 
 		echo "+------------------------------------------------------------+"
 		echo "| Update OS and clean OS                                     |"
@@ -110,9 +132,9 @@ if [ $(id -u) -eq 0 ];
 
 		# Update OS
 		apt update
-		apt upgrade -y
-		apt autoclean -y
-		apt autoremove -y
+		apt upgrade --yes
+		apt autoclean --yes
+		apt autoremove --yes
 
 
 		echo "+------------------------------------------------------------+"
@@ -122,7 +144,12 @@ if [ $(id -u) -eq 0 ];
 
 		if [ -f /var/run/reboot-required ]; then
 			echo ' Redemarrage dans 10 secondes - Ctrl+C pour annuler'
-			sleep 10
+			secondes=10
+			for i in $(seq $secondes -1 1); do
+    				echo "$i seconds to reboot...";
+    				sleep 1;
+			done;
+			echo "reboot now!"
 			reboot
 		fi
 
